153cdfb5ac4e83cc9151e87a359659a5c7b89a63
JYNIKI <1067087283@qq.com>
2021-01-10 12:43:01 +0800

    107

--- code/inference/__init__.py
+++
@@ -1,5 +0,0 @@
-'''
-Author: Niki
-Date: 2021-01-07 13:49:38
-Description: 
-'''
--- code/training/learning_rate/poly_lr.py
+++
@@ -1,7 +0,0 @@
-'''
-Author: Niki
-Date: 2021-01-08 11:00:07
-Description: 
-'''
-def poly_lr(epoch, max_epochs, initial_lr, exponent=0.9):
-    return initial_lr * (1 - epoch / max_epochs)**exponent
