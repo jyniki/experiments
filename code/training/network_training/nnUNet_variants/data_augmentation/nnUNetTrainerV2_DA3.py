526ca3c8d14d4f262a2bb2d4fd4f0c6dfb13b446
JYNIKI <1067087283@qq.com>
2021-01-10 17:08:35 +0800

    .

--- code/training/network_training/nnUNet_variants/data_augmentation/nnUNetTrainerV2_DA3.py
+++ code/training/network_training/nnUNet_variants/data_augmentation/nnUNetTrainerV2_DA3.py
@@ -1,16 +1,3 @@
-#    Copyright 2020 Division of Medical Image Computing, German Cancer Research Center (DKFZ), Heidelberg, Germany
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
 import torch
 from batchgenerators.dataloading import MultiThreadedAugmenter, SingleThreadedAugmenter
 from batchgenerators.transforms import Compose, MirrorTransform, GammaTransform, BrightnessTransform, \
@@ -32,7 +19,7 @@ from training.data_augmentation.downsampling import DownsampleSegForDSTransform3
 from training.data_augmentation.pyramid_augmentations import \
     RemoveRandomConnectedComponentFromOneHotEncodingTransform, ApplyRandomBinaryOperatorTransform, MoveSegAsOneHotToData
 from training.dataloading.dataset_loading import unpack_dataset
-from training.loss_functions.deep_supervision import MultipleOutputLoss2
+from loss_functions.deep_supervision import MultipleOutputLoss2
 from training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2, maybe_mkdir_p
 from utils import softmax_helper
 from torch import nn
